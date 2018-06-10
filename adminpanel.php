c76badc6a49394c07bdfbd9c58629fdadd2c60e4
Sachintha <ravindusachintha53@gmail.com>
2018-06-10 16:44:24 +0530

    update repo

---
+++ adminpanel.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Admin Panel</title>
+</head>
+<body>
+
+</body>
+</html>
--- mycart.php
+++ mycart.php
@@ -1,3 +1,14 @@
+<?php
+
+session_start();
+
+if(!(isset($_SESSION['usertype'])&&($_SESSION['usertype']=2))){
+	header("location: login.php");
+}
+
+?>
+
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -50,13 +61,3 @@
 </div>
 </body>
 </html>
-=======
-<?php
-
-session_start();
-
-if(!(isset($_SESSION['usertype'])&&($_SESSION['usertype']=2))){
-	header("location: login.php");
-}
-
-?>
